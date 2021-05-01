11f5c9293a88fdcd49b984825847ea9b62a4d746
manolia <62712893+manolia@users.noreply.github.com>
2021-05-01 17:45:01 +0200

    Template project

---
+++ Stealer/Stealer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Stealer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
